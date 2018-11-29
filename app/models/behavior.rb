59ab1af6df956605f2272bb9f351c64268938920
Pye-Casey <caseypye@byui.edu>
2018-11-28 20:45:22 -0700

    behavior index now gets the correct student by id

--- app/models/behavior.rb
+++ app/models/behavior.rb
@@ -1,13 +1,13 @@
 class Behavior < ApplicationRecord
 	has_one :student
-
+	
 	def readable_time
 		event_time.strftime("%I:%M%p")  
 	end
 
 	def student_name
-		student = Student.find(student_id)
-		student_name = student.full_name_last
+		Student.find(student_id).full_name_last
 	end
+
 
 end
