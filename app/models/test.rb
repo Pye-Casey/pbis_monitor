1f8695094e765141ed89a4e03f2c178afd8c7664
Pye-Casey <caseypye@byui.edu>
2018-12-06 22:16:53 -0700

    latest events

--- app/models/test.rb
+++ app/models/test.rb
@@ -1,5 +1,9 @@
 class Test < ApplicationRecord
 	
+	@recentEvents = Behavior.order(:event_date, :event_time).where(event_date: 1.week.ago..Date.today)
 
+  	@eventsByDay = Behavior.group_by_day(:event_date).count
+
+  	@highFliers = Behavior.order(:event_date, :event_time).where(event_date: 1.week.ago..Date.today)
 
 end
