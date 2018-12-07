be096bd656d926c46d061c01a9607cd8000add85
Pye-Casey <caseypye@byui.edu>
2018-12-07 14:21:42 -0700

    adding table to admin

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -1,4 +1,11 @@
 class AdminController < ApplicationController
   def index
+  	@students = Student.all
+  	@behavior = Behavior.all
+  	@recentEvents = Behavior.order(:event_date, :event_time).where(event_date: 1.week.ago..Date.today).limit(10)
+
+  	@eventsByDay = Behavior.group_by_day(:event_date).count
+  	@highFlierCnt = Behavior.select('distinct student_id').count
+  	@highFliers = Behavior.select(:student_id, 'COUNT(*) as count').group(:student_id).where('student_id IS NOT NULL').order('count')
   end
 end
