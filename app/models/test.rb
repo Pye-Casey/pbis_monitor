2fb0771b085d63703f252a7cda26e39a75737b43
Pye-Casey <caseypye@byui.edu>
2018-12-06 22:28:01 -0700

    latest events

--- app/models/test.rb
+++ app/models/test.rb
@@ -1,9 +1,20 @@
 class Test < ApplicationRecord
-	
-	@recentEvents = Behavior.order(:event_date, :event_time).where(event_date: 1.week.ago..Date.today)
+	has_many :behavior
+	has_many :student
+	@recentEvents = Behavior.order(:event_date, :event_time).where(event_date: 1.week.ago..Date.today).limit(10)
 
   	@eventsByDay = Behavior.group_by_day(:event_date).count
 
-  	@highFliers = Behavior.order(:event_date, :event_time).where(event_date: 1.week.ago..Date.today)
+  	def student_name
+		# check if there is a student id
+		if student_id
+			Student.find(student_id).full_name_last
+		else
+			"unknown"
+		end
+
+	end
+
+  	@highFliers = Behavior.count('student_id', :distinct => true)
 
 end
