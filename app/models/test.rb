74382a681e3ca97af840f04f0c61a781f9a4d8f4
Pye-Casey <caseypye@byui.edu>
2018-12-06 21:36:13 -0700

    latest events

--- app/models/test.rb
+++ app/models/test.rb
@@ -1,8 +1,6 @@
 class Test < ApplicationRecord
-	has_many: behavior
+	has_many: behaviors
 	has_many: students
-#	@behavior = Behavior.find(:all).all? { |e|  }
-	test_behavior = Behavior.find(0)
-	@behavior = Behavior.all? { |e|  }
+
 
 end
