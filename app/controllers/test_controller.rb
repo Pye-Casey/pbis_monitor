cca6b1825523d2e4191b0858dc9f7dc9c69e8019
Pye-Casey <caseypye@byui.edu>
2018-12-06 21:24:14 -0700

    latest events

--- app/controllers/test_controller.rb
+++ app/controllers/test_controller.rb
@@ -1,4 +1,7 @@
 class TestController < ApplicationController
   def index
+  	@students = Student.all
+  	@behavior = Behavior.all
+  	@recentEvents = Behavior.order(:event_date, :event_time).where(event_date: 1.week.ago..Date.today)
   end
 end
--- app/models/test.rb
+++ app/models/test.rb
@@ -1,2 +1,8 @@
 class Test < ApplicationRecord
+	has_many: behaviors
+	has_many: students
+#	@behavior = Behavior.find(:all).all? { |e|  }
+	test_behavior = Behavior.find(0)
+	@behavior = Behavior.all? { |e|  }
+
 end
