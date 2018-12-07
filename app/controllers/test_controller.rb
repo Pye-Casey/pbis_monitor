4d8293ebf4c5435ead759ae2ec6225fd6ebdda97
Pye-Casey <caseypye@byui.edu>
2018-12-06 21:59:21 -0700

    latest events

--- app/controllers/test_controller.rb
+++ app/controllers/test_controller.rb
@@ -3,5 +3,7 @@ class TestController < ApplicationController
   	@students = Student.all
   	@behavior = Behavior.all
   	@recentEvents = Behavior.order(:event_date, :event_time).where(event_date: 1.week.ago..Date.today)
+
+  	@eventsByDay = Behavior.group_by_day(:event_date).count
   end
 end
