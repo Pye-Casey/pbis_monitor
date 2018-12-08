2be0155b97863dedcf17348b4158a6069f034a5c
Pye-Casey <caseypye@byui.edu>
2018-12-08 09:38:12 -0700

    charts with real data

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -9,7 +9,7 @@ class AdminController < ApplicationController
   	@highFliers = Behavior.select(:student_id, 'COUNT(*) as count').group(:student_id).where('student_id IS NOT NULL').order('count')
 
   	@weekData = Behavior.select(:event_date, 'COUNT(*) as count').group(:event_date).where(event_date: 1.week.ago..Date.today)
-
+  	@monthData = Behavior.select(:event_date, 'COUNT(*) as count').group(:event_date).where(event_date: 1.month.ago..Date.today)
 
   	@weekDataTest = Behavior.select('event_date, COUNT(*) as count').group(:event_date)
 
