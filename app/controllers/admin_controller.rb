e70ad4cf006296f76b5df529f078d5abdb4cd20f
Pye-Casey <caseypye@byui.edu>
2018-12-08 09:20:26 -0700

    first morris chart working

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -7,5 +7,11 @@ class AdminController < ApplicationController
   	@eventsByDay = Behavior.group_by_day(:event_date).count
   	@highFlierCnt = Behavior.select('distinct student_id').count
   	@highFliers = Behavior.select(:student_id, 'COUNT(*) as count').group(:student_id).where('student_id IS NOT NULL').order('count')
+
+  	@weekData = Behavior.select(:event_date, 'COUNT(*) as count').group(:event_date).where(event_date: 1.week.ago..Date.today)
+
+
+  	@weekDataTest = Behavior.select('event_date, COUNT(*) as count').group(:event_date)
+
   end
 end
