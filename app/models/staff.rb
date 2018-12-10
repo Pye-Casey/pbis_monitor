365d82198cdfe63c9f84dde018c962b791d2aa40
Damon Simpkinson <damonsimpkinson@gmail.com>
2018-12-10 15:46:27 -0500

    add password requirement back to staff

--- app/models/staff.rb
+++ app/models/staff.rb
@@ -12,8 +12,8 @@ class Staff < ApplicationRecord
                     format: { with: VALID_EMAIL_REGEX },
                     uniqueness: { case_sensitive: false }
 
-  #has_secure_password
-  #validates :password, presence: true, length: { minimum: 6 }
+  has_secure_password
+  validates :password, presence: true, length: { minimum: 6 }
 
 	def full_name_last
 		"#{lName}, #{fName}"
