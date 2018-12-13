26b3719c72081c41718c085d07a83c7bfb9a6856
Pye-Casey <caseypye@byui.edu>
2018-12-12 21:04:52 -0700

    tables in desc order

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -5,12 +5,14 @@ class AdminController < ApplicationController
   def index
   	@students = Student.all
   	@behavior = Behavior.all
-  	@recentEvents = Behavior.order(:event_date, :event_time).where(event_date: 1.week.ago..Date.today).limit(10)
+  	#@recentEvents = Behavior.order(:event_date, :event_time).where(event_date: 1.week.ago..Date.today).limit(10)
+    @recentEvents = Behavior.order("event_date DESC", "event_time DESC").where(event_date: 1.week.ago..Date.today).limit(10)
 
   	@eventsByDay = Behavior.group_by_day(:event_date).count
   	@highFlierCnt = Behavior.select('distinct student_id').count
-  	@highFliers = Behavior.select(:student_id, 'COUNT(*) as count').group(:student_id).where('student_id IS NOT NULL').order('count')
-    #@highFliers = Behavior.select(:student_id,'COUNT(*) as count', 'student_name').group(:student_id, :student_name).where('student_name IS NOT NULL').order('count')
+  	#@highFliers = Behavior.select(:student_id, 'COUNT(*) as count').group(:student_id).where('student_id IS NOT NULL').order('count')
+    @highFliers = Behavior.select(:student_id, 'COUNT(*) as count').group(:student_id).where('student_id IS NOT NULL').order('count desc')
+
   	@weekData = Behavior.select(:event_date, 'COUNT(*) as count').group(:event_date).where(event_date: 1.week.ago..Date.today)
   	@monthData = Behavior.select(:event_date, 'COUNT(*) as count').group(:event_date).where(event_date: 1.month.ago..Date.today)
 
