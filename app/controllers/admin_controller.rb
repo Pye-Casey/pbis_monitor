afa31bb9aadef88a9cc0b42b0e1345045faa3867
Pye-Casey <caseypye@byui.edu>
2018-12-12 19:05:18 -0700

    behavior index fix

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -10,7 +10,7 @@ class AdminController < ApplicationController
   	@eventsByDay = Behavior.group_by_day(:event_date).count
   	@highFlierCnt = Behavior.select('distinct student_id').count
   	@highFliers = Behavior.select(:student_id, 'COUNT(*) as count').group(:student_id).where('student_id IS NOT NULL').order('count')
-
+    #@highFliers = Behavior.select(:student_id,'COUNT(*) as count', 'student_name').group(:student_id, :student_name).where('student_name IS NOT NULL').order('count')
   	@weekData = Behavior.select(:event_date, 'COUNT(*) as count').group(:event_date).where(event_date: 1.week.ago..Date.today)
   	@monthData = Behavior.select(:event_date, 'COUNT(*) as count').group(:event_date).where(event_date: 1.month.ago..Date.today)
 
--- app/models/behavior.rb
+++ app/models/behavior.rb
@@ -5,7 +5,31 @@ class Behavior < ApplicationRecord
 		event_time.strftime("%I:%M%p")  
 	end
 
-	
+	def create_student_name
+		# check if there is a student id
+		if student_id
+			# is it valid?
+			begin
+				Student.find(student_id).full_name_last
+			rescue Exception => e
+				"unknown"
+			end
+			
+		else
+			"unknown"
+		end
+
+	end
+
+def create_staff_name
+		# check if there is a student id
+		if staff_id
+			Staff.find(staff_id).full_name_last
+		else
+			"unknown"
+		end
+
+	end
 
 	
 
